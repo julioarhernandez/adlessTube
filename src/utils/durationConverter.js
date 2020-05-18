ac049c36f6444ac9774a2ad07f6cffbc6115aace
Julio Rodriguez <juliorodriguez@ncl.com>
2020-05-18 11:08:50 -0400

    Duration converter function refactoring

--- src/utils/durationConverter.js
+++ src/utils/durationConverter.js
@@ -1,20 +1,33 @@
 const durationConverter = (time) => {
+    const addCeros = (items) => {
+        if (items.length > 1){
+           const ceroPadded = items.map((v,i) => {
+               return (i === 0  ? v : v.padStart(2,'0'));
+           });
+           return ceroPadded;
+        }else{
+            const tempItems = [];
+            tempItems.push(`${items[0]}S`);
+            return tempItems;
+        }
+    };
     if (time === "PT0S" || time === "P0D"){
-            return "LIVE";
+        return "LIVE";
+    }
+    if (time) {
+        var a = time.match(/\d+/g);
+        if (time.indexOf('M') >= 0 && time.indexOf('H') === -1 && time.indexOf('S') === -1) {
+            a = [0, a[0], 0];
         }
-        if (time) {
-            var a = time.match(/\d+/g);
-            if (time.indexOf('M') >= 0 && time.indexOf('H') === -1 && time.indexOf('S') === -1) {
-                a = [0, a[0], 0];
-            }
-            if (time.indexOf('H') >= 0 && time.indexOf('M') === -1) {
-                a = [a[0], 0, a[1]];
-            }
-            if (time.indexOf('H') >= 0 && time.indexOf('M') === -1 && time.indexOf('S') === -1) {
-                a = [a[0], 0, 0];
-            }
-            return a.join(':');
+        if (time.indexOf('H') >= 0 && time.indexOf('M') === -1) {
+            a = [a[0], 0, a[1]];
         }
-        return;
+        if (time.indexOf('H') >= 0 && time.indexOf('M') === -1 && time.indexOf('S') === -1) {
+            a = [a[0], 0, 0];
+        }
+        let b = addCeros(a);
+        return b.join(':');
+    }
+    return;
 };
 export default durationConverter;
